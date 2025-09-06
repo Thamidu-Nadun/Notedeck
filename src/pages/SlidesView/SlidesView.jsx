38ee8cd74e425fe08aef5c66de08f22e94a61969
Thamidu-Nadun <nadunrz101@gmail.com>
2025-09-07 00:52:18 +0530

    Fixed minor issue in import path

--- src/pages/SlidesView/SlidesView.jsx
+++ src/pages/SlidesView/SlidesView.jsx
@@ -1,5 +1,5 @@
 import {useLocation} from 'react-router-dom';
-import SlideWrapper from '../../components/SlideWrapper/SlideWrapper';
+import SlideWrapper from '../../Components/SlideWrapper/SlideWrapper';
 
 const mdContent = `
 # Slide 1
