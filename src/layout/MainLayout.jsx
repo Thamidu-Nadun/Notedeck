d22f8a04c9c427d79bcd7ce9e52c3810a0bdd0dc
Thamidu-Nadun <nadunrz101@gmail.com>
2025-09-07 00:56:48 +0530

    Fixed minor issue in import path

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,5 +1,5 @@
-import Navbar from '../components/Navbar/Navbar';
-import Footer from '../components/Footer/Footer';
+import Navbar from '../Components/Navbar/Navbar';
+import Footer from '../Components/Footer/Footer';
 
 const MainLayout = ({children}) => {
   return (
